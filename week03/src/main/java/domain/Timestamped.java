9c833f61d61e402d4b71d899560323b367f7bca8
happine2s <happine2s@sookmyung.ac.kr>
2022-09-30 01:07:15 +0900

    03. Repository 만들기

---
+++ week03/src/main/java/domain/Timestamped.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass //객체를 자동으로 칼럼으로 인식
+@EntityListeners(AuditingEntityListener.class) //생성과 변경 시간을 자동 업데이트
+public class Timestamped {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
