38668361a4034e7644c42279c0931d08c927d8b1
happine2s <happine2s@sookmyung.ac.kr>
2022-10-04 15:13:10 +0900

    07. 관심 상품 등록하기

--- week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -2,8 +2,11 @@ package com.sparta.week04.controller;
 
 import com.sparta.week04.domain.Product;
 import com.sparta.week04.domain.ProductRepository;
+import com.sparta.week04.domain.ProductRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +16,17 @@ import java.util.List;
 public class ProductRestController {
     private final ProductRepository productRepository;
 
+    //등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts(){
         return productRepository.findAll();
+    }
+
+    //신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto){
+        Product product=new Product(requestDto);
+        productRepository.save(product);
+        return product;
     }
 }
