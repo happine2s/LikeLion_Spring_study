b0263a6ab25ee3264cd545c6e88da764a8cd34de
happine2s <happine2s@sookmyung.ac.kr>
2022-10-04 16:01:49 +0900

    08. 키워드로 상품 검색하기

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,10 +1,18 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.domain.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component //@RequiredArgsConstructor와 함께 사용할 경우 스프링이 자동으로 생성
 public class NaverShopSearch {
-    public String search(){
+    public String search(String query){
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "WGChfn_vHpV0LnAiFNrb");
@@ -12,7 +20,7 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=adidas", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -22,8 +30,24 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args){
-        NaverShopSearch naverShopSearch=new NaverShopSearch();
-        naverShopSearch.search();
+    public List<ItemDto> fromJSONtoItem(String result){
+        JSONObject rjson=new JSONObject(result);
+        JSONArray items=rjson.getJSONArray("items");
+        List<ItemDto> ret=new ArrayList<>();
+
+        for(int i=0;i<items.length();i++){
+            JSONObject itemJson=(JSONObject) items.get(i);
+            System.out.println(itemJson);
+            ItemDto itemDto=new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+
+        return ret;
     }
+
+//    public static void main(String[] args){
+//        NaverShopSearch naverShopSearch=new NaverShopSearch();
+//        String ret=naverShopSearch.search("아이맥");
+//        naverShopSearch.fromJSONtoItem(ret);
+//    }
 }
